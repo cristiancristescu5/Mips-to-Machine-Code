5cc7ab9c017276673ba88c50f8899e4f48900ccd
Cristian Cristescu <cristian.cristescu27@gmail.com>
2023-09-15 14:18:31 +0300

    bau

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,11 +3,11 @@ package org.example;
 import java.util.Arrays;
 
 public class Main {
-    public final static Interpreter interpreter = new Interpreter("C:\\Users\\crist\\OneDrive\\Desktop\\mipsToMachineCode\\src\\main\\resources\\instructions.txt",
-                                                                "C:\\Users\\crist\\OneDrive\\Desktop\\mipsToMachineCode\\src\\main\\resources\\machineCodeInstructions.dat");
+    public final static Interpreter interpreter = new Interpreter("src\\main\\resources\\instructions.txt",
+                                                                "src\\main\\resources\\machineCodeInstructions.dat");
     public static void main(String[] args) {
         interpreter.interpret();
-        System.out.println(interpreter.toString());
+        System.out.println(interpreter);
         interpreter.writeFile();
     }
 }
